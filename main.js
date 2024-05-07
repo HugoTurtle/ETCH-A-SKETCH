fc926f8839da57afda0aad223d7092f32f274bd1
Hugo Perez <hugopereztm12309@gmail.com>
2024-05-07 09:25:07 -0400

    Fix an error by creating the right amount of divs
    Previously had a logic error where the amount of divs created inside the
    container didn't reflect the amount needed for the dimensions. For
    example running the function createGridOfSquareDivs(4) would only create
    8 divs when in reality the needed amount is 16(4 by 4 grid).

--- main.js
+++ main.js
@@ -2,7 +2,9 @@ const container = document.querySelector("#container");
 
 function createGridOfSquareDivs(gridDimension) {
 
-    for(let i = 0; i < gridDimension * 2; ++i) {
+    gridDimension *= gridDimension;
+
+    for(let i = 0; i < gridDimension; ++i) {
         const squareDiv = document.createElement("div");
         squareDiv.classList.toggle("square");
         container.appendChild(squareDiv);
