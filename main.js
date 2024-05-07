ea5d13057ab2de0161c7d70d5da12ed937231d10
Hugo Perez <hugopereztm12309@gmail.com>
2024-05-06 20:29:48 -0400

    Create the amount of divs needed for the container
    The divs are yet to be visible in a fashion to make them look like a
    grid.

---
+++ main.js
@@ -0,0 +1,11 @@
+const container = document.querySelector("#container");
+
+function createGridOfSquareDivs(gridDimension) {
+
+    for(let i = 0; i < gridDimension * 2; ++i) {
+        const squareDiv = document.createElement("div");
+        container.appendChild(squareDiv);
+    }
+
+}
+createGridOfSquareDivs(16);
