99a79db55a52729c3895ee3c3e9f75627350aa34
Hugo Perez <hugopereztm12309@gmail.com>
2024-05-06 20:55:42 -0400

    Make the divs visible with borders

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ function createGridOfSquareDivs(gridDimension) {
 
     for(let i = 0; i < gridDimension * 2; ++i) {
         const squareDiv = document.createElement("div");
+        squareDiv.classList.toggle("square");
         container.appendChild(squareDiv);
     }
 
