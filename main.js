ac9e631751d02319a66f12ad1830b038f03586c7
Hugo Perez <hugopereztm12309@gmail.com>
2024-05-07 19:41:02 -0400

    Dynamically calculate the size of each square
    This method guarantees that all squares will be equal in size and will
    neatly fill up the container, maintaining the grid structure.

--- main.js
+++ main.js
@@ -1,14 +1,18 @@
 const container = document.querySelector("#container");
+const WIDTH_OF_CONTAINER = 550;
 
 function createGridOfSquareDivs(gridDimension) {
 
-    gridDimension *= gridDimension;
+    const totalSquares = gridDimension * gridDimension;
+    const squareSize = WIDTH_OF_CONTAINER / gridDimension;
 
-    for(let i = 0; i < gridDimension; ++i) {
+    for(let i = 0; i < totalSquares; ++i) {
         const squareDiv = document.createElement("div");
         squareDiv.classList.toggle("square");
+        squareDiv.style.width = squareSize + "px";
+        squareDiv.style.height = squareSize + "px";
         container.appendChild(squareDiv);
     }
 
 }
-createGridOfSquareDivs(16);
+createGridOfSquareDivs(2);
